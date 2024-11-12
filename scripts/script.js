161ca76fb6d6e6524d917c99d59643a494c1c93d
Chris4536 <59596505+Chris4536@users.noreply.github.com>
2024-11-12 16:54:55 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+const slides = document.getElementsByClassName('carousel__item');
+const slidesTotal = slides.length;
+const nextButton = document.querySelector('#carousel__button--next');
+const prevButton = document.querySelector('#carousel__button--prev');
+
+
+let moviePoster = document.querySelector('.movie-poster');
+
+let modal = document.querySelector('#myModal');
+
+let closedBtn = document.querySelector('.closeBtn');
+
+let modalImage = document.querySelector('.movie-poster-modal');
+
+let slidePosition = 0;
+
+moviePoster.addEventListener('click', ()=> {
+    modal.style.display = 'block';
+    modalImage.src = moviePoster.src; 
+});
+
+closedBtn.addEventListener('click', () =>{
+    modal.style.display = 'none'; 
+
+});
+
+window.addEventListener('click', (event) => {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    for(let slide of slides){
+        slide.classList.remove('carousel__item--visible');
+    }
+
+    if(slidePosition === slidesTotal - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
+});
+
+prevButton.addEventListener('click', () => {
+    for(let slide of slides){
+        slide.classList.remove('carousel__item--visible');
+    }
+
+    if(slidePosition === 0) {
+        slidePosition = slidesTotal - 1;
+    } else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
+});
